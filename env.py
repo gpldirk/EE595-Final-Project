9254e0bed8a03604cb73d4384a734fbcbe60b3e1
Max <gepeilu@usc.edu>
2020-04-07 15:23:15 -0700

    update env

--- env.py
+++ env.py
@@ -1,63 +1,77 @@
+import numpy as np
 
 #           small  medium  large
 # vCPU        1      2       4
 # memory(GB)  1      4       8
-# tasks under the same job should be on the same sevrer ?
+# Timestamp, cpu requested, memory requested
+# type: [cpu, memory]
+virtual_machine_repository = {
+  'small': [1, 1],
+  'medium': [2, 4],
+  'large': [4, 8]
+}
 
-class VirtualMachine(object):
-  def __init__(self, vm_Type, required_Cpu = 0, required_Memory = 0):
-    self.vm_Type = vm_Type
-    self.required_Cpu = required_Cpu
-    self.required_Memory = required_Memory
-
+an = 1
+bn = 1
+optimal_utilization = 1
+server_cpu = 4
+server_memory = 8
+serversOfCluster = 1000
+numOfClusters = 10
 
 class Task(object):
-  def __init__(self, request_Cpu, request_Memory, int timestamp): # ???
-    self.jobId = jobId
-    self.taskId = taskId
-    self.request_Cpu = request_Cpu
-    self.request_Memory = request_Memory
-    self.request_vm = request_vm
-    self.priority = priority
-    self.ddl = ddl 
+  def __init__(self, timestamp, requested_cpu, requested_memory):
+    self.timestamp = timestamp
+    self.requested_cpu = requested_cpu
+    self.requested_memory = requested_memory
+
+class VirtualMachine(object):
+  def __init__(self, vm_type, required_cpu = 0, required_memory = 0):
+    self.vm_type = vm_type
+    self.cpu = virtual_machine_repository[vm_type][0]
+    self.memory = virtual_machine_repository[vm_type][1]
+    self.required_cpu = required_cpu
+    self.required_memory = required_memory
     
 class Server(object):
-  def __init__(self, Cpu, Memory, numOfVM, vm):
-    self.Cpu = Cpu
-    self.Memory = Memory
-    self.numOfVM = numOfVM
-    self.vm_Type = vm.vm_Type
-    self.vm_list = list() # 
+  def __init__(self, cpu, memory, vm_type):
+    self.cpu = cpu
+    self.memory = memory
+    self.vm_type = vm_type
+    self.numOfVM = min(self.cpu // virtual_machine_repository[vm_type][0], 
+                       self.memory // virtual_machine_repository[vm_type][1])
+    self.vm_list = []
     for i in range(numOfVM):
-      self.vm_list.append(VirtualMachine(vm.vm_Type))
+      self.vm_list.append(VirtualMachine(vm_type))
   
-  # a, b and optimal_utilization
-  def UtilizationOfCPU(self):
+  def CPU_Utilization_Rate(self):
     utilize_cpu = 0
     for vm in self.vm_list:
-      utilize_cpu += vm.required_Cpu
-    utilization_rate = utilize_cpu / self.Cpu
-    return a * utilization_rate if utilization_rate < optimal_utilization 
-      else a * utilization_rate + ((utilization_rate - optimal_utilization) ** 2) * b
+      utilize_cpu += vm.required_cpu
+    utilization_rate = utilize_cpu / self.cpu
+    return utilization_rate * an if utilization_rate < optimal_utilization \
+      else utilization_rate * an + ((utilization_rate - optimal_utilization) ** 2) * bn
 
 class Cluster(object):
   def __init__(self):
+    self.cpu = 0
+    self.memory = 0
     self.servers = []
-    self.Cpu = 0
-    self.Memory = 0
-    for i in range(n):
-
-      self.servers.append(Server())
-
+    for i in range(serversOfCluster):
+      # randomly choose the vm_type for current server
+      vm_type = np.random.choice(virtual_machine_repository.keys())
+      self.servers.append(Server(server_cpu, server_memory, vm_type))
 
 
-cluster c1 = Cluster()
 class Environment(object):
   def __init__(self):
- 
-  
+    self.clusters = []
+    for i in range(numOfClusters):
+      self.clusters.append(Cluster())
+    
   def reset(self, layer):
-    self.__init__(layer)
+    self.layer = layer
+    self.__init__()
 
   def Reward(layer, action):
     if layer == 1:
@@ -71,5 +85,7 @@ class Environment(object):
   # new_observation, reward, reject, option = env.step(action)
   def step(self, action):
     reward = Reward(self.layer, action)
+
+
     
 
